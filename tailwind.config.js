166ad85e14d1a37e0356ebe661501856f312b3bf
hyejungg <kimhyejung12@naver.com>
2025-07-15 22:12:47 +0900

    tailwind css 설정 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true,
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        SUIT: ['SUIT'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    tailwindcss()
+  ],
 }
